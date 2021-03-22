ea0804f71b6f821d854d11508c953f7a2e00ebcb
Andy Cork <andy@wearemothership.com>
2021-03-22 14:48:43 +0000

    Hook setup

--- dist/usePDF.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-declare const usePDF: ({ source, loadingImage, quality, enableAnnotations }: IUsePDF) => {
-    changeZoom: ({ scale, viewer, scrollContainer }: IChangeZoom) => void;
-    pages: JSX.Element[];
-};
-export { usePDF };
---
+++ example/src/App.tsx
@@ -0,0 +1,64 @@
+import React, { SyntheticEvent, useRef, useState } from 'react';
+import { usePDF } from "pdf-viewer";
+
+const App = (): JSX.Element => {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+	const viewerRef = useRef<HTMLDivElement>(null);
+	const [zoom, setZoom] = useState<number>(1);
+	const [file, setFile] = useState<Uint8Array>();
+	const sourceOptions = {
+		data: file
+	}
+	const { pages, changeZoom } = usePDF({
+		source: sourceOptions
+	})
+	const ZOOM_CHANGE: number = 0.1;
+
+	const zoomIn = () => {
+		const newZoom = zoom + ZOOM_CHANGE;
+		if (newZoom > 0) {
+			setZoom(newZoom);
+			changeZoom({ scale: newZoom, scrollContainer: scrollContainerRef.current, viewer: viewerRef.current })
+		}
+	}
+
+	const zoomOut = () => {
+		const newZoom = zoom - ZOOM_CHANGE;
+		if (newZoom < 2) {
+			setZoom(newZoom);
+			changeZoom({ scale: newZoom, scrollContainer: scrollContainerRef.current, viewer: viewerRef.current })
+		}
+	}
+
+	const fileChanged = (ev: SyntheticEvent<HTMLInputElement>) => {
+		const target = ev.target as HTMLInputElement;
+		const file = target.files?.[0]
+		//Step 3:Read the file as ArrayBuffer
+		if (file) {
+			//Step 2: Read the file using file reader
+			var fileReader = new FileReader();  
+
+			fileReader.onload = (e) => {
+				const { result } = e.target as FileReader;
+				const arr = new Uint8Array(result as ArrayBuffer);
+				setFile(arr);
+			};
+			fileReader.readAsArrayBuffer(file);
+		}
+	}
+
+	return (
+		<div className="App">
+			<input type="file" onChange={fileChanged} />
+			<div id="scrollContainer" ref={scrollContainerRef} style={{ overflow: "scroll", height: "800px" }}>
+				<div id="viewer" ref={viewerRef}>
+					{pages}
+				</div>
+			</div>
+			<button type="button" onClick={zoomIn}>+</button>
+			<button type="button" onClick={zoomOut}>-</button>
+		</div>
+	);
+}
+
+export default App;
