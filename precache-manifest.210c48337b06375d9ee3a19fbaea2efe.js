8890258de6f2bba7e7d3eec47e0bcfd8405aaa77
Andy Cork <andy@wearemothership.com>
2021-03-22 14:58:10 +0000

    Updates

---
+++ precache-manifest.210c48337b06375d9ee3a19fbaea2efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95056bed03d225d1b06f1cd71970b176",
+    "url": "/pdf-viewer/index.html"
+  },
+  {
+    "revision": "16721421dd0356c48046",
+    "url": "/pdf-viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b79787123ba0e76ec7e1",
+    "url": "/pdf-viewer/static/js/2.a75a61fa.chunk.js"
+  },
+  {
+    "revision": "1ddbd254a787d39e201f6a3dc31e5b17",
+    "url": "/pdf-viewer/static/js/2.a75a61fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16721421dd0356c48046",
+    "url": "/pdf-viewer/static/js/main.b009d466.chunk.js"
+  },
+  {
+    "revision": "8f36eeabf194e85b82d8",
+    "url": "/pdf-viewer/static/js/runtime-main.a8fcbbcf.js"
+  }
+]);
