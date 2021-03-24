e3f09bd38895c1ba167406632025b7b91627915a
Andy Cork <andy@wearemothership.com>
2021-03-24 14:31:08 +0000

    linting

--- src/components/PlaceholderPage.tsx
+++ src/components/PlaceholderPage.tsx
@@ -1,12 +1,14 @@
-import styles from "./styles/PlaceholderPage.module.css";
 import React from "react";
 import Spinner from "../assets/spinner.gif";
+import styles from "./styles/PlaceholderPage.module.css";
 
-const PlaceholderPage = ({ width, height, type = "place", loadingImage = Spinner, spin }: IPlaceholderPage): JSX.Element => {
+const PlaceholderPage = ({
+	width, height, type = "place", loadingImage = Spinner, spin
+}: IPlaceholderPage): JSX.Element => {
 	const classes = [
 		spin ? styles.spinner : "",
 		styles.centered
-	]
+	];
 	return (
 		<div style={{ width: `${width}px`, height: `${height}px` }} data-type={type}>
 			<img src={loadingImage} className={classes.join(" ")} alt="Loading..." />
