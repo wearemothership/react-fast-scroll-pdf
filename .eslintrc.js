c6f28a23206681c1a82aaca14992f6869d229dcb
Andy Cork <andy@wearemothership.com>
2021-04-07 10:22:20 +0100

    no-shadow

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,7 +57,6 @@ module.exports = {
 		"react/jsx-fragments": [2, "syntax"],
 		"no-use-before-define": "off",
 		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"no-shadow": "off",
 	},
 	parserOptions: {
 		sourceType: "module"
@@ -79,6 +78,7 @@ module.exports = {
 			rules: {
 				"@typescript-eslint/explicit-module-boundary-types": ["error"],
 				"@typescript-eslint/no-use-before-define": ["error"],
+				"no-shadow": "off",
 				"@typescript-eslint/no-shadow": "error"
 			}
 		}
