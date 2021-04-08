692375a277f3275b652e2a9e073f629b66cd7b15
Andy Cork <andy@wearemothership.com>
2021-04-08 12:55:59 +0100

    Example PDF button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,35 @@
 import "./App.css";
 import React, { SyntheticEvent, useState } from "react";
 import { FastScrollPDF } from "react-fast-scroll-pdf";
-import { GoClippy, GoMarkGithub } from "react-icons/go";
+import { GoClippy, GoMarkGithub, GoFilePdf } from "react-icons/go";
+import example1 from "./example1.pdf";
+
+interface IExampleFileButton {
+	fileName: string,
+	selectedFile?: string,
+	file: string,
+	loadFile: (fileName: string, file: string) => void
+}
+
+const ExampleFileButton = ({
+	fileName, selectedFile, loadFile, file
+}: IExampleFileButton) => {
+	const selected = fileName === selectedFile;
+	return (
+		<button type="button" onClick={() => loadFile(fileName, file)} className={selected ? "selected" : ""}>
+			<GoFilePdf />
+			{fileName}
+		</button>
+	);
+};
+
+ExampleFileButton.defaultProps = {
+	selectedFile: ""
+};
 
 const App = (): JSX.Element => {
 	const [file, setFile] = useState<Uint8Array>();
+	const [fileName, setFileName] = useState<string>();
 	const [hideZoom, setHideZoom] = useState<boolean>(false);
 	const sourceOptions = {
 		data: file
@@ -29,9 +54,18 @@ const App = (): JSX.Element => {
 				const { result } = e.target as FileReader;
 				const arr = new Uint8Array(result as ArrayBuffer);
 				setFile(arr);
+				setFileName(newFile.name);
 			};
 			fileReader.readAsArrayBuffer(newFile);
 		}
+	};
+
+	const loadFile = (newFileName: string, newFile: string) => {
+		fetch(newFile).then((response) => response.arrayBuffer().then((buff) => {
+			const arr = new Uint8Array(buff as ArrayBuffer);
+			setFile(arr);
+			setFileName(newFileName);
+		}));
 	};
 
 	return (
@@ -63,10 +97,17 @@ const App = (): JSX.Element => {
 					</div>
 				</div>
 			</section>
+			<section>
+				<div className="flex flexCenterRow flexWrap">
+					<div className="buttons">
+						<ExampleFileButton fileName="example1.pdf" selectedFile={fileName} loadFile={loadFile} file={example1} />
+					</div>
+					<input name="pdfFile" type="file" onChange={fileChanged} />
+				</div>
+			</section>
 
 			<section>
 				<div className="flex flexCenterRow flexSpaced100">
-					<input name="pdfFile" type="file" onChange={fileChanged} />
 					<label htmlFor="hideZoom">
 						<small>Zoom Buttons: </small>
 						<input name="hideZoom" type="checkbox" checked={hideZoom} onChange={() => setHideZoom(!hideZoom)} />
