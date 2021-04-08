1488aba053ef687673d92c19b9a111157e345172
Andy Cork <andy@wearemothership.com>
2021-04-08 10:27:29 +0100

    Remove flex component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,7 @@
 import "./App.css";
 import React, { SyntheticEvent, useState } from "react";
-import { Link } from "react-router-dom";
 import { FastScrollPDF } from "react-fast-scroll-pdf";
 import { GoClippy, GoMarkGithub } from "react-icons/go";
-import Flex from "./components/Flex";
 
 const App = (): JSX.Element => {
 	const [file, setFile] = useState<Uint8Array>();
@@ -39,13 +37,10 @@ const App = (): JSX.Element => {
 	return (
 		<div className="App">
 			<section>
-				<Flex>
+				<div className="flex">
 					<h1>react-fast-scroll-pdf</h1>
 					<p>A fast, efficient PDF renderer.</p>
-					<Flex
-						flexDirection="row"
-						alignItems="center"
-					>
+					<div className="flex flexCenterRow">
 						<button
 							type="button"
 							onClick={() => {
@@ -65,33 +60,22 @@ const App = (): JSX.Element => {
 							npm install --save react-fast-scroll-pdf
 						</button>
 						{copied && <small>Copied…</small>}
-					</Flex>
-				</Flex>
+					</div>
+				</div>
 			</section>
 
 			<section>
-				<Flex
-					flexDirection="row"
-					alignItems="center"
-					justifyContent="space-between"
-					width="100%"
-				>
+				<div className="flex flexCenterRow flexSpaced100">
 					<input name="pdfFile" type="file" onChange={fileChanged} />
 					<label htmlFor="hideZoom">
 						<small>Zoom Buttons: </small>
 						<input name="hideZoom" type="checkbox" checked={hideZoom} onChange={() => setHideZoom(!hideZoom)} />
 					</label>
-				</Flex>
+				</div>
 			</section>
 
 			<section>
-				<Flex
-					flexDirection="row"
-					justifyContent="center"
-					width="100%"
-					backgroundColor="rgb(242, 242, 242)"
-					padding="2rem"
-				>
+				<div className="flex flexCenterRow pdfScrollContainer">
 					{ file
 						? (
 							<FastScrollPDF
@@ -101,15 +85,11 @@ const App = (): JSX.Element => {
 							/>
 						)
 						: null }
-				</Flex>
+				</div>
 			</section>
 
 			<section>
-				<Flex
-					flexDirection="row"
-					alignItems="center"
-					flexWrap="wrap"
-				>
+				<div className="flex flexCenterRow flexWrap">
 					<button
 						type="button"
 						onClick={() => {
@@ -120,28 +100,10 @@ const App = (): JSX.Element => {
 							Made by Mothership
 						</small>
 					</button>
-				</Flex>
+				</div>
 			</section>
 
 		</div>
-	// <div className={styles.app}>
-	// 	<div className={styles.settingsDiv}>
-	// 		<label htmlFor="pdfFile">
-	// 			<span>Choose File: </span>
-	// 			<input name="pdfFile" type="file" onChange={fileChanged} />
-	// 		</label>
-	// 		<label htmlFor="hideZoom">
-	// 			<span>Hide Zoom Buttons: </span>
-	// 			<input name="hideZoom" type="checkbox" className={styles.valid} checked={hideZoom}
-	// onChange={() => setHideZoom(!hideZoom)} />
-	// 		</label>
-	// 	</div>
-	// 	{ file
-	// 		? (
-	// 			<FastScrollPDF className={styles.fastScroll} source={sourceOptions} hideZoom={hideZoom} />
-	// 		)
-	// 		: null }
-	// </div>
 	);
 };
 
