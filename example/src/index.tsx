62664f7cb9cf8c8ac924ea7f470f4e9bf6ed3d9c
Andy Cork <andy.cork@gmail.com>
2022-10-13 08:18:03 +0100

    Update example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// eslint-disable-next-line import/extensions
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
